af52a1a5aa101339e476a12fbeef7cd13cc887b6
Gogs <gogs@fake.local>
2015-05-20 11:19:03 -0400

    Search.ByName now searches child tags, updated examples

--- document_test.go
+++ document_test.go
@@ -4,9 +4,8 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 	"testing"
 
-	"strings"
-
 	"fmt"
+	"strings"
 )
 
 const (
@@ -246,7 +245,7 @@ func ExampleNewDocumentFromReader() {
 	<foo>
 		<bar>bat</bar>
 		<baz/>
-		<fizz><![CDATA[&lt;cdata&gt;contents&lt;/cdata&gt;]]></fizz>
+		<fizz><![CDATA[&lt;foo&gt;contents&lt;/foo&gt;]]></fizz>
 	</foo>
 </root>
 <!-- comment below root element -->`
@@ -257,10 +256,18 @@ func ExampleNewDocumentFromReader() {
 		panic(err)
 	}
 
-	// get the root tag
-	root := doc.Root()
+	// get the fizz tag and value
+	fizz := doc.Root().Search().ByName("foo").ByName("fizz").One()
+	if fizz == nil {
+		panic("fizz is missing")
+	}
 
-	fmt.Println(len(root.Elements()))
+	fv, err := fizz.Value()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("fizz: ", fv)
 	//Output:
-	//2
+	//fizz:  <foo>contents</foo>
 }
